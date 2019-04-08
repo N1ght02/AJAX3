12fb2bd7a0827e2ba5926f4da8458fe0886d35f1
Flint <43080255+N1ght02@users.noreply.github.com>
2019-04-08 11:52:26 +0200

    Add files via upload

---
+++ dbVars.php
@@ -0,0 +1,14 @@
+
+<?php
+$servername = "127.0.0.1";
+$uid 		= "c3668flint2";
+$pwd		= "yUo#4fQSfrvERJ";
+$database 	= "c3668test2";
+
+//
+//$servername = "localhost";
+//$uid 		= "root";
+//$pwd		= "";
+//$database 	= "test2";
+//
+?>
